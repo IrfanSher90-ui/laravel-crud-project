d1cdc846a7926ff3bc2877f0dd3ee34108c6b0d5
IrfanSher90-ui <sherirfan90@gmail.com>
2026-03-01 05:12:10 -0800

    Add files via upload

---
+++ storage/framework/views/ff25e1d92b54ffc18d6a6b7c5d6a6f2e.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome Over Page</h1>
+    <a href="<?php echo e(route('welcome')); ?>">Welcome Page</a>
+
+</body>
+</html><?php /**PATH C:\Users\skill master\crudapp\resources\views/home.blade.php ENDPATH**/ ?>
